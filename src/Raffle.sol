dfae609ace1ffb6d7cd2b880bf98c7ba81411bd9
gabriel.ritta <gabriel.ritta@defensoria.ba.def.br>
2025-07-15 17:28:07 -0300

    comentando guia de estilo solidity

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,27 @@
+// Layout of the contract file:
+// version
+// imports
+// errors
+// interfaces, libraries, contract
+​
+// Inside Contract:
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+​
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
