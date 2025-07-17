519e8aced4adb014b5fd09d9927e84ce415c8ed7
gabrielfst30 <gabrielfst30@gmail.com>
2025-07-16 21:59:59 -0300

    feature: criando error personalizado e definindo event para address que entrar no sorteio.

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,14 +3,14 @@
 // imports
 // errors
 // interfaces, libraries, contract
-​
+
 // Inside Contract:
 // Type declarations
 // State variables
 // Events
 // Modifiers
 // Functions
-​
+
 // Layout of Functions:
 // constructor
 // receive function (if exists)
@@ -33,22 +33,46 @@ pragma solidity 0.8.19;
 */
 
 contract Raffle {
+    /** Errors */
+    error Raffle__SendMoreToEnterRaffle(); //Error personalizado
+
+    /** Events */
+    event RaffleEntered(address indexed player);
+
+    /** Variables */
     uint256 private immutable i_entranceFee; //taxa de entrada
 
-    //Inicializando com a taxa de entrada
+    //address[] precisa ser payable para que eu possa transferir ETH para o vencedor
+    address payable[] private s_players; //array de jogadores - storage variable
+
+    // Inicializando com a taxa de entrada
     constructor(uint256 entraceFee){
         i_entranceFee = entraceFee;
     }
 
-    //Entrar no sorteio
-    function enterRaffle() public payable {}
+    // Entrar no sorteio
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!"); 
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); 
+        // Se não tiver valor minimo de taxa de entrada
+        if(msg.value < i_entranceFee){
+            revert Raffle__SendMoreToEnterRaffle();
+        }
 
-    //Pegar vencedor
+        //Adicionando quem entrou no sorteio a array s_players
+        s_players.push(payable(msg.sender));
+
+        //emitindo um evento para quando o player entrar no sorteio
+        emit RaffleEntered(msg.sender);
+        
+    }
+
+    // Pegar vencedor
     function pickWinner() public {}
 
     /** Getter Functions */
 
-    //Get taxa de entrada
+    // Get taxa de entrada
     function getEntraceFee() external view returns(uint256){
         return i_entranceFee;
     }
