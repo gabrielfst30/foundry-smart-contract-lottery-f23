3068ef918471dff8ad84b50f9869c0bb7ef5a1d3
gabrielfst30 <gabrielfst30@gmail.com>
2025-07-19 19:05:28 -0300

    feature: criando condição de intevalo para rodar o pickWinner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -21,61 +21,71 @@
 // private
 // view & pure functions
 
-
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-/** 
-* @title Raffle Contract
-* @author Gabriel Santa Ritta
-* @notice Creating a sample raffle
-* @dev Implements Chainlink VRFv2.5
-*/
+/**
+ * @title Raffle Contract
+ * @author Gabriel Santa Ritta
+ * @notice Creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
 
 contract Raffle {
     /** Errors */
-    error Raffle__SendMoreToEnterRaffle(); //Error personalizado
+    error Raffle__SendMoreToEnterRaffle(); //@dev error personalizado
 
     /** Events */
     event RaffleEntered(address indexed player);
 
     /** Variables */
-    uint256 private immutable i_entranceFee; //taxa de entrada
-
+    uint256 private immutable i_entranceFee; //@dev taxa de entrada
+    uint256 private immutable i_interval; //@dev duração da lotéria em segundos
     //address[] precisa ser payable para que eu possa transferir ETH para o vencedor
-    address payable[] private s_players; //array de jogadores - storage variable
+    address payable[] private s_players; //@dev array de jogadores - storage variable
+    uint256 private s_lastTimeStamp; //@dev timestamp inicial pós implementação de contrato
 
     // Inicializando com a taxa de entrada
-    constructor(uint256 entraceFee){
+    constructor(uint256 entraceFee, uint256 interval) {
         i_entranceFee = entraceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     // Entrar no sorteio
-    function enterRaffle() public payable {
-        // require(msg.value >= i_entranceFee, "Not enough ETH sent!"); 
-        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle()); 
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
         // Se não tiver valor minimo de taxa de entrada
-        if(msg.value < i_entranceFee){
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
 
         //Adicionando quem entrou no sorteio a array s_players
-        s_players.push(payable(msg.sender));
+        s_players.push(payable(msg.sender)); //msg.sender = address que interagiu com o contrato
 
         //emitindo um evento para quando o player entrar no sorteio
         emit RaffleEntered(msg.sender);
-        
     }
 
     // Pegar vencedor
-    function pickWinner() public {}
+    /** 1. Pegar um número randomico
+        2. Usar o número randomico to pick a player
+        3. Chamar automáticamente 
+    */
+    function pickWinner() external {
+        //@@dev timestamp atual - timestamp contarto < intervalo
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        //Pegue nosso random numer da Chainlink
+    }
 
     /** Getter Functions */
 
     // Get taxa de entrada
-    function getEntraceFee() external view returns(uint256){
+    function getEntraceFee() external view returns (uint256) {
         return i_entranceFee;
     }
-
-    
 }
