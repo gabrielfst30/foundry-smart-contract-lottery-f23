75063aa80eb4c313083dc8a80cd501d7ecca3c71
gabrielfst30 <gabrielfst30@gmail.com>
2025-08-22 01:01:58 -0300

    feature: criando introdução do deploy

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract DeployRafflle is Script {
+    function run() public {}
+
+    // function deployContract() public returns (Raffle, HelperConfig) {}
+}
