cb493727f1cc7c8aed4f2b76ed10f3c2500c438d
gabrielfst30 <gabrielfst30@gmail.com>
2025-08-26 01:40:59 -0300

    feature: configurando o deploy do contrato baseado no chainid

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,8 +4,27 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
 contract DeployRafflle is Script {
     function run() public {}
 
-    // function deployContract() public returns (Raffle, HelperConfig) {}
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // instanciando helperConfig
+        // local -> deploy mocks, pega local config
+        // sepolia -> pega sepolia config
+        HelperConfig.NetworkConfig memory config = helperConfig.getConfig(); // chamando getConfig() que trará nosso block.chainid
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            config.entranceFee,
+            config.interval,
+            config.vrfCoordinator,
+            config.gasLane,
+            config.subscriptionId,
+            config.callbackGasLimit
+        );
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
 }
