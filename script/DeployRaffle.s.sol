1ed4bbff1cc5c1533c0fe328f7f1663e1a7008b3
gabrielfst30 <gabrielfst30@gmail.com>
2025-09-07 02:30:34 -0300

    feature: configs de addConsumer e fundSubscription

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,15 +5,29 @@ import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 
 import {HelperConfig} from "script/HelperConfig.s.sol";
-
-contract DeployRafflle is Script {
-    function run() public {}
+import {CreateSubscription} from "script/Interactions.s.sol";
+import {FundSubscription, AddConsumer} from "script/Interactions.s.sol";
+contract DeployRaffle is Script {
+    function run() public {
+        deployContract(); // chamando a função que fará o deploy
+    }
 
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig(); // instanciando helperConfig
         // local -> deploy mocks, pega local config
         // sepolia -> pega sepolia config
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig(); // chamando getConfig() que trará nosso block.chainid
+
+        // se o subscriptionId for 0, cria uma subscription nova (serve para podermos fazer testes locais)
+        if(config.subscriptionId == 0){
+            CreateSubscription subscriptionContract = new CreateSubscription(); // instanciando o contrato que criará a subscription
+            (config.subscriptionId, config.vrfCoordinator) = subscriptionContract.createSubscription(config.vrfCoordinator); // criando a subscription passando o vrfCoordinator do config
+            // Acima, retornamos o subId e o vrfCoordinator e setamos na nossa config
+        
+        // Fund a subscription se for local
+        FundSubscription fundSubscription = new FundSubscription(); // instanciando o contrato que criará a subscription
+        fundSubscription.fundSubscription(config.vrfCoordinator, config.subscriptionId, config.link); // fundando a subscription)
+        }
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -25,6 +39,10 @@ contract DeployRafflle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer(); // instanciando o contrato que adicionará o consumer
+        addConsumer.addConsumer(address(raffle), config.vrfCoordinator, config.subscriptionId); // adicionando o consumer
+
         return (raffle, helperConfig);
     }
 }
